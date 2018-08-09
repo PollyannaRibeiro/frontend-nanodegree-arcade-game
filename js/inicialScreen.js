a766483c9f0d5cff9c7d446007fb3ca3ad0199f2
Polly <recpolly@gmail.com>
2018-08-09 13:22:57 +0100

    Improving the initial screen style

--- js/inicialScreen.js
+++ js/inicialScreen.js
@@ -4,10 +4,11 @@ const container = document.getElementById('container');
 const characteres = ['images/char-boy.png', "images/char-cat-girl.png", "images/char-horn-girl.png", "images/char-pink-girl.png", "images/char-princess-girl.png"];
 
 function inicialScreen(){
+
     const screen = document.createElement('div');
     screen.classList.add("screen");
     container.appendChild(screen);
-    const p = document.createElement('p');
+    const p = document.createElement('h1');
     p.textContent = "Choose your character:";
     p.setAttribute('id', 'info');
     screen.appendChild(p);
@@ -23,12 +24,14 @@ function inicialScreen(){
     selectingCharacter();
 
     const p2 = document.createElement('p');
-    p2.textContent = "You have 3 lifes, good luck!";
+    p2.textContent = "Take the player to the water avoiding the insects.";
+    const p3 = document.createElement('p');
+    p3.textContent = "You have 3 chances, good luck!";
     screen.appendChild(p2);
+    screen.appendChild(p3);
 
 }
-
-
+// selecting characterer and starting the game
 
 function selectingCharacter(){
     const definedCharacter = document.getElementsByClassName('characteres');
